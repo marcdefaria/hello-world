cc1bd87b44c407b578f5b6a371a85dcf6a79459c
Marc De Faria <marcdefaria@gmail.com>
2019-11-24 21:52:51 +0000

    Square module module and export function used in index.js

--- src/test-node/index.js
+++ src/test-node/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+var square = require('./square')
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
   console.log('Received a get request and responded')
+  console.log('4 * 4 is equal to: ' + square.area(4))
 });
+
+/*app.get('calculate', function(req,res) {
+	var answer = square.area(4)
+	res.send('The answer to 4*4 is: ' + answer)
+});*/
 
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!');
